f62407e226ebb4677dbe76b8cc7d251ae0c22a00
23a_srm <23a_srm@3003-8>
2024-10-24 14:43:26 +0300

    Add project files.

---
+++ project_1/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace project_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.health = new System.Windows.Forms.Label();
+            this.coins = new System.Windows.Forms.Label();
+            this.power = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(579, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MAGIC DREAMS";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(570, 651);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "PLAY";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // health
+            // 
+            this.health.AutoSize = true;
+            this.health.Location = new System.Drawing.Point(36, 61);
+            this.health.Name = "health";
+            this.health.Size = new System.Drawing.Size(38, 13);
+            this.health.TabIndex = 3;
+            this.health.Text = "Health";
+            this.health.Visible = false;
+            this.health.Click += new System.EventHandler(this.health_Click);
+            // 
+            // coins
+            // 
+            this.coins.AutoSize = true;
+            this.coins.Location = new System.Drawing.Point(38, 91);
+            this.coins.Name = "coins";
+            this.coins.Size = new System.Drawing.Size(36, 13);
+            this.coins.TabIndex = 4;
+            this.coins.Text = "Coins:";
+            this.coins.Visible = false;
+            // 
+            // power
+            // 
+            this.power.AutoSize = true;
+            this.power.Location = new System.Drawing.Point(38, 115);
+            this.power.Name = "power";
+            this.power.Size = new System.Drawing.Size(40, 13);
+            this.power.TabIndex = 5;
+            this.power.Text = "Power:";
+            this.power.Visible = false;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button2.Location = new System.Drawing.Point(22, 519);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(482, 189);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Fight";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(777, 519);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(482, 189);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Run";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(22, 519);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(482, 189);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(777, 519);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(482, 189);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Visible = false;
+            // 
+            // button6
+            // 
+            this.button6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button6.Location = new System.Drawing.Point(1226, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(42, 35);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Х";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.power);
+            this.Controls.Add(this.coins);
+            this.Controls.Add(this.health);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label health;
+        private System.Windows.Forms.Label coins;
+        private System.Windows.Forms.Label power;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ project_1/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_1
+{
+    public partial class Form1 : Form
+    {
+        private int hp;
+        private int cns;
+        private int pwr;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            coins.Visible = true;
+            health.Visible = true;
+            power.Visible = true;
+            button1.Visible = false;
+            button2.Visible = true;
+            button3.Visible = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hp = 100;
+            cns = 10;
+            pwr = 25;
+            health.Text = "Health: " + hp.ToString();
+            coins.Text = "Coins: " + cns.ToString();
+            power.Text = "Power: " + pwr.ToString();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            hp -= 10;
+            cns -= 5;
+            pwr -= 7;
+
+
+            health.Text = "Health: " + hp.ToString();
+            coins.Text = "Coins: " + cns.ToString();
+            power.Text = "Power: " + pwr.ToString();
+
+
+            if (hp <= 0)
+            {
+                health.Text = "Health: 0 (Game Over)";
+            }
+        }
+
+
+
+        private void health_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = true;
+            button5.Visible = true;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
